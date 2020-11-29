963d904a736d2a9143302aaad4b2f1285269e8c5
Arthur Eichelberger <eichelbergerarthur@gmail.com>
2020-11-29 11:08:16 +0100

    :sparkles: Storing time in the Chrome Local Storage.

--- app/helpers/Storage.js
+++ app/helpers/Storage.js
@@ -3,14 +3,23 @@
  */
 const Storage = () => {
 
+    /**
+     * Get an item from the Chrome Local Storage.
+     * @param {string} key 
+     */
     const getItem = (key) => new Promise((resolve) => {
         chrome.storage.local.get(null, (data) => {
             resolve(data[key] || undefined)
         })
     })
 
+    /**
+     * Set an item in the Chrome Local Storage.
+     * @param {string} key 
+     * @param {any} value 
+     */
     const setItem = (key, value) => new Promise((resolve) => {
-        chrome.storage.sync.set({key: value}, () => {
+        chrome.storage.local.set({[key]: value}, () => {
             resolve(value)
         });
     })
