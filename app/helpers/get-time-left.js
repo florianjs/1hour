73fc4ebd3983a65cb087ddf05c41c8152353e0e5
Judikaelle <pierre.gicquel@protonmail.ch>
2020-12-13 10:22:31 +0100

    Fix the timer display when the time is over

--- app/helpers/get-time-left.js
+++ app/helpers/get-time-left.js
@@ -9,7 +9,7 @@ const getTimeLeft = (timeStamp, delay) => {
   const hours = Math.floor(timeLeft / 60 / 60);
   const minutes = Math.floor(timeLeft / 60) - hours * 60;
   const seconds = timeLeft % 60;
-  const timeIsUp = !hours && !minutes && !seconds;
+  const timeIsUp = timeLeft <= 0;
   return timeIsUp
     ? 'Time is up!'
     : `${hours ? hours + 'h ' : ''}${minutes ? minutes + 'min ' : ''}${
